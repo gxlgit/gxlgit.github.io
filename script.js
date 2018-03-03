0919b8fba6507dcf2be646e8f1b40c045e73326b
Gwen Latasa <gxlatasa@gmail.com>
2018-03-03 11:19:19 -0500

    sticky nav on scroll

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#sticky-nav').pushpin({
+      top: $('#sticky-nav').offset().top
+    });
+    // $('.scrollspy').scrollSpy({
+    //   scrollOffset: 0
+    // });
+  });
+
+console.log('hello,gwen');
