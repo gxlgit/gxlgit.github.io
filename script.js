9e5c00434f996b7731913eef32ca119cd2d6e9c3
Gwen Latasa <gxlatasa@gmail.com>
2018-03-04 15:04:34 -0500

    better nav bar and mobile responsiveness

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
     $('#sticky-nav').pushpin({
+
       top: $('#sticky-nav').offset().top
     });
     // $('.scrollspy').scrollSpy({
     //   scrollOffset: 0
     // });
+$(".button-collapse").sideNav();
+
   });
 
 console.log('hello,gwen');
